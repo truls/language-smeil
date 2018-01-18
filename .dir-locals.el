0845aa701ed53f946a7bb62734fb524016656f6f
Truls Asheim <truls@asheim.dk>
2018-01-18 23:48:38 +0100

    Emacs dir-local setting intero targets

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (intero-targets "language-smeil:lib" "language-smeil:exe:smeast" "language-smeil:test:libsme-test")))
